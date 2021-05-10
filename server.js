fe1413bef8ff5c4ec6c9126ea27870e77067fec1
v8phan <56612604+v8phan@users.noreply.github.com>
2021-05-10 17:51:16 -0500

    read operation
    getting quotes from mongodb, working on displaying quotes in html with ejs

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ MongoClient.connect("mongodb://localhost:27017/crem", {
   useUnifiedTopology: true,
 }).then((client) => {
   console.log("Connected to Database");
-  const db = client.db("quotes");
+  const db = client.db("star-wars-quotes");
+  const quotesCollection = db.collection('quotes')
+
+  app.set('view engine', 'ejs')
+
   app.use(express.urlencoded({ extended: true }));
 
   app.listen(3000, function () {
@@ -16,10 +20,23 @@ MongoClient.connect("mongodb://localhost:27017/crem", {
   })
 
   app.get("/", (req, res) => {
+    const cursor = db.collection('quotes').find().toArray()
+      .then(results => {
+        res.render('index.ejs', { quotes: results })
+      })
+      .catch(/*...*/)
+    console.log(cursor)
     res.sendFile(__dirname + "/index.html");
   })
 
   app.post("/quotes", (req, res) => {
+    quotesCollection.insertOne(req.body)
+      .then(result => {
+        console.log(result)
+        res.render(view, locals)
+        res.redirect('/')
+      })
+    .catch(error => console.error(error))
     console.log(req.body);
   })
 })
@@ -28,3 +45,7 @@ MongoClient.connect("mongodb://localhost:27017/crem", {
 //test commit
 
 //npm run dev to trigger nodemon server.js
+
+//Next, we’ll use res.render to render this index.ejs file.
+
+
